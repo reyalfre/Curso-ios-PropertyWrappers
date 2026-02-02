e808d899070dbd8b2f0a01d0a1b92f9713a5f37a
Alfredo Maldonado <alfredorafael1995@hotmail.com>
2026-02-02 10:34:39 +0100

    Initial Commit

---
+++ Curso-ios-PropertyWrappers/Curso_ios_PropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_ios_PropertyWrappersApp.swift
+//  Curso-ios-PropertyWrappers
+//
+//  Created by Equipo 8 on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_ios_PropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
